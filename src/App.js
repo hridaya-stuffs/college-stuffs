231b868f23c280d903787b7150cf659cff9c8af6
Hridaya <info.hridayaprajapati@gmail.com>
2023-12-01 10:49:52 +0545

    my-apps: Update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,73 @@
-// import logo from "./logo.svg";
 import "./App.css";
 import { Fragment, useEffect, useState } from "react";
-import Header from "./components/header";
 
 function App() {
   const [dogList, setDogList] = useState([]);
+  const [dogImage, setDogImage] = useState("");
+  const [allImage, setAllImage] = useState([]);
 
   useEffect(() => {
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch("https://dog.ceo/api/breeds/list/all", requestOptions)
-      .then((response) => response.json())
-      .then((result) => setDogList(Object.keys(result.message)))
-      .catch((error) => console.log("error", error));
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then((res) => res.json())
+      .then((result) => {
+        const breeds = Object.keys(result.message);
+        setDogList(breeds);
+      });
   }, []);
+  const singleRandomDog = (dogName) => {
+    fetch("https://dog.ceo/api/breed/" + dogName + "/images/random")
+      .then((res) => res.json())
+      .then((result) => {
+        setDogImage(result.message);
+        setAllImage([]);
+      });
+  };
+  const allDog = (dogName) => {
+    fetch("https://dog.ceo/api/breed/" + dogName + "/images")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setDogImage("");
+        setAllImage(result.message);
+      });
+  };
 
-  const dataList = dogList.map((item, index) => {
-    return <li key={index}>{item}</li>;
+  const dataList = dogList.map((item) => {
+    return (
+      <li key={item}>
+        <span
+          onClick={() => {
+            singleRandomDog(item);
+          }}
+        >
+          {item}
+        </span>{" "}
+        <button
+          onClick={() => {
+            allDog(item);
+          }}
+        >
+          all images
+        </button>
+      </li>
+    );
   });
+  const showAllDogs = allImage.map((item, key) => {
+    return <img key={key} src={item} />;
+  });
+  console.log(dogList);
 
   return (
     <Fragment>
-      <Header />
-      <ul>{dataList}</ul>
+      <div className="dog-list-wrapper">
+        <ol>{dataList}</ol>
+        <div className="image-list">
+          <img
+            src={dogImage ? dogImage : "https://dog.ceo//img/dog-api-logo.svg"}
+          />
+          {showAllDogs}
+        </div>
+      </div>
     </Fragment>
   );
 }
