b39547e4b1ea00728a881320c943fea97d4f6058
Hridaya <info.hridayaprajapati@gmail.com>
2023-12-04 00:07:52 +0545

    [TMP] Add navBarItems

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,8 +1,25 @@
+import "./NavigationBar.css";
+
 const NavigationBar = () => {
+  const home = "Home";
+  const about = "About";
+  const contact = "Contact";
+  const projects = "Projects";
+  const resume = "Resume";
+  const blog = "Blog";
+  const links = [home, about, contact, projects, resume, blog];
+
+  const listItems = links.map((link, index) => (
+    <a key={index} href={"#" + link}>
+      {link}
+    </a>
+  ));
+
   return (
-    <nav className="navbar">
+    <div className="navbar">
       <h1>React</h1>
-    </nav>
+      <div className="navBarItems">{listItems}</div>
+    </div>
   );
 };
 
